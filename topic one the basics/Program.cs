d5b7ce3d5a71cb5d964767744582d74920fe2266
339720419 <339720419@2260I-L29-003>
2024-09-05 09:29:12 -0400

    Add project files.

---
+++ topic one the basics/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace topic_one_the_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //parker hammond
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("i will print on a new line");
+            Console.WriteLine();
+            Console.Write("hello world");
+            Console.WriteLine("what is skib");
+            Console.ReadLine();
+        }
+    }
+}
